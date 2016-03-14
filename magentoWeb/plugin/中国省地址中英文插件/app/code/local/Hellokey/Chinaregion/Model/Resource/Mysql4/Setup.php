5c00d4dc4e080c396c99fadf4f67c5a14925336b
guohuadeng <guohuadeng@hotmail.com>
2016-03-14 23:09:19 +0800

    stampcustomer插件改下样式，将弹出窗口放到按键下方

---
+++ magentoWeb/plugin/中国省地址中英文插件/app/code/local/Hellokey/Chinaregion/Model/Resource/Mysql4/Setup.php
@@ -0,0 +1,6 @@
+<?php
+
+class Hellokey_Chinaregion_Model_Resource_Mysql4_Setup extends Mage_Core_Model_Resource_Setup{
+
+
+}
---
+++ magentoWeb/plugin/中国省地址中英文插件/app/code/local/Hellokey/Chinaregion/sql/chinaregion_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,61 @@
+<?php
+$installer = $this;
+$data = array(
+array('CN','BJ','北京', 'Beijing'),
+array('CN','GD','广东', 'Guangdong'),
+array('CN','SH','上海', 'Shanghai'),
+array('CN','TJ','天津', 'Tianjin'),
+array('CN','HE','河北', 'Hebei'),
+array('CN','SX','山西', 'Shanxi'),
+array('CN','NM','内蒙古', 'Inner Mongolia'),
+array('CN','LN','辽宁', 'Liaoning'),
+array('CN','JL','吉林', 'Jilin'),
+array('CN','HL','黑龙江', 'Heilongjiang'),
+array('CN','JS','江苏', 'Jiangsu'),
+array('CN','ZJ','浙江', 'Zhejiang'),
+array('CN','AH','安徽', 'Anhui'),
+array('CN','FJ','福建', 'Fujian'),
+array('CN','JX','江西', 'Jiangxi'),
+array('CN','SD','山东', 'Shandong'),
+array('CN','HA','河南', 'Henan'),
+array('CN','HB','湖北', 'Hubei'),
+array('CN','HN','湖南', 'Hunan'),
+array('CN','GX','广西', 'Guangxi'),
+array('CN','HI','海南', 'Hainan'),
+array('CN','CQ','重庆', 'Chongqing'),
+array('CN','SC','四川', 'Sichuan'),
+array('CN','GZ','贵州', 'Guizhou'),
+array('CN','YN','云南', 'Yunnan'),
+array('CN','XZ','西藏', 'Tibet'),
+array('CN','SN','陕西', 'Shaanxi'),
+array('CN','GS','甘肃', 'Gansu'),
+array('CN','QH','青海', 'Qinghai'),
+array('CN','NX','宁夏', 'Ningxia'),
+array('CN','XJ','新疆', 'Xinjiang'),
+array('CN','HK','香港', 'Hong Kong'),
+array('CN','AM','澳门', 'Macau'),
+array('CN','TW','台湾', 'Taiwan')
+);
+foreach ($data as $row) {
+    $bind = array(
+        'country_id'    => $row[0],
+        'code'          => $row[1],
+        'default_name'  => $row[3],
+    );
+    $installer->getConnection()->insert($installer->getTable('directory/country_region'), $bind);
+    $regionId = $installer->getConnection()->lastInsertId($installer->getTable('directory/country_region'));
+
+    $bind = array(
+        'locale'    => 'en_US',
+        'region_id' => $regionId,
+        'name'      => $row[3]
+    );
+    $installer->getConnection()->insert($installer->getTable('directory/country_region_name'), $bind);
+    $bind = array(
+        'locale'    => 'zh_CN',
+        'region_id' => $regionId,
+        'name'      => $row[2]
+    );
+    $installer->getConnection()->insert($installer->getTable('directory/country_region_name'), $bind);
+}
+
